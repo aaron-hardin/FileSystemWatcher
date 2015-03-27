0a96e96ad3e5635d6a7da45f84667082f8e4548a
aaron-hardin <teslasprotege@gmail.com>
2015-03-26 19:19:24 -0500

    adding snap in
    don't know where the extra plugin loader came from

---
+++ FileWatcherSnapIn/ConfigurationFormView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ManagementConsole;
+
+namespace FileWatcherSnapIn
+{
+	public class ConfigurationFormView : FormView
+	{
+	}
+}
